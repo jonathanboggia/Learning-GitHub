c73fd7e759feccf25cb9c66bd998e7af4f5a57f4
Jonathan Boggia <jonathan.boggia@icloud.com>
2021-11-03 19:02:46 +0100

    Initial Commit

---
+++ Lesson_02_GitHub&Xcode/Lesson_02_GitHub_XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_02_GitHub_XcodeApp.swift
+//  Lesson_02_GitHub&Xcode
+//
+//  Created by Jonathan Boggia on 03.11.21.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_02_GitHub_XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
